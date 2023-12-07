75b87bcdfcb5d9f2644f0f321e573985fb334e83
Alex Gillon <alexandergillon@outlook.com>
2023-12-06 21:47:12 -0500

    Adds handling for 'same station' constraints. Fixes issues with tube_data.txt.

--- scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/AmplDriver.java
+++ scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/AmplDriver.java
@@ -28,11 +28,19 @@ public class AmplDriver {
     /** AMPL data file. */
     private BufferedWriter amplDatFile;
 
+    /** Line width of a metro line on the map, in pixels. */
+    private final int metroLineWidth;
+
+    /** X/Y offset of two stations that are diagonally adjacent. */
+    private final int diagonalOffset;
+
     /**
      * @param initialModelPath Path to the initial AMPL model file.
      */
-    public AmplDriver(String initialModelPath) {
+    public AmplDriver(String initialModelPath, int metroLineWidth) {
         this.initialModelPath = initialModelPath;
+        this.metroLineWidth = metroLineWidth;
+        this.diagonalOffset = (int)Math.floor(((double)metroLineWidth) / Math.sqrt(2));
     }
 
     /**
@@ -175,7 +183,6 @@ public class AmplDriver {
      */
     private void processCardinalDirectionConstraint(String inputText, String constraintType, MetroLine metroLine,
                                                     Map<String, MetroLine> metroLines, int textLineNumber) throws IOException {
-        // TODO: this code is duplicate of some other code somewhere - pull out into Util
         Pair<String, String> doubleQuotedResult = Util.consumeDoubleQuoted(inputText, textLineNumber);
         String stationsString = doubleQuotedResult.getLeft();
         stationsString = stationsString.strip();
@@ -183,6 +190,148 @@ public class AmplDriver {
         List<Pair<String, String>> stationPairs = Util.allConsecutiveStationPairs(stationsString, textLineNumber);
         for (Pair<String, String> stationPair : stationPairs) {
             writeCardinalDirectionConstraint(stationPair.getLeft(), stationPair.getRight(), constraintType, metroLine, metroLines, textLineNumber);
+        }
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations, where one is directly above the other, to the .mod file.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     */
+    private void writeSameStationAboveConstraint(String station1Id, String station2Id) throws IOException {
+        writeVerticalConstraint(station1Id, station2Id);
+        amplModFile.write(String.format("subject to same_station_above_%s_%s: SOLVED_Y_COORDS[%s] + %d = SOLVED_Y_COORDS[%s];",
+                station1Id, station2Id, station1Id, metroLineWidth, station2Id));
+        amplModFile.newLine();
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations, where one is directly left of the other, to the .mod file.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     */
+    private void writeSameStationLeftConstraint(String station1Id, String station2Id) throws IOException {
+        writeHorizontalConstraint(station1Id, station2Id);
+        amplModFile.write(String.format("subject to same_station_left_%s_%s: SOLVED_X_COORDS[%s] + %d = SOLVED_X_COORDS[%s];",
+                station1Id, station2Id, station1Id, metroLineWidth, station2Id));
+        amplModFile.newLine();
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations, where one is directly above and to the right of the other, to the .mod file.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     */
+    private void writeSameStationAboveRightConstraint(String station1Id, String station2Id) throws IOException {
+        amplModFile.write(String.format("subject to same_station_above_right_above_%s_%s: SOLVED_Y_COORDS[%s] + %d = SOLVED_Y_COORDS[%s];",
+                station1Id, station2Id, station1Id, diagonalOffset, station2Id));
+        amplModFile.newLine();
+        amplModFile.write(String.format("subject to same_station_above_right_right_%s_%s: SOLVED_X_COORDS[%s] - %d = SOLVED_X_COORDS[%s];",
+                station1Id, station2Id, station1Id, diagonalOffset, station2Id));
+        amplModFile.newLine();
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations, where one is directly above and to the left of the other, to the .mod file.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     */
+    private void writeSameStationAboveLeftConstraint(String station1Id, String station2Id) throws IOException {
+        amplModFile.write(String.format("subject to same_station_above_left_above_%s_%s: SOLVED_Y_COORDS[%s] + %d = SOLVED_Y_COORDS[%s];",
+                station1Id, station2Id, station1Id, diagonalOffset, station2Id));
+        amplModFile.newLine();
+        amplModFile.write(String.format("subject to same_station_above_left_left_%s_%s: SOLVED_X_COORDS[%s] + %d = SOLVED_X_COORDS[%s];",
+                station1Id, station2Id, station1Id, diagonalOffset, station2Id));
+        amplModFile.newLine();
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations, where both are in the exact same location, to the .mod file.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     */
+    private void writeSameStationEqualConstraint(String station1Id, String station2Id) throws IOException {
+        amplModFile.write(String.format("subject to same_station_equal_x_%s_%s: SOLVED_X_COORDS[%s] = SOLVED_X_COORDS[%s];",
+                station1Id, station2Id, station1Id, station2Id));
+        amplModFile.newLine();
+        amplModFile.write(String.format("subject to same_station_equal_y_%s_%s: SOLVED_Y_COORDS[%s] = SOLVED_Y_COORDS[%s];",
+                station1Id, station2Id, station1Id, station2Id));
+        amplModFile.newLine();
+    }
+
+    /**
+     * Writes a 'same station' constraint between two stations.
+     * @param station1Id Identifier of the first station, in the STATIONS AMPL set.
+     * @param station2Id Identifier of the second station, in the STATIONS AMPL set.
+     * @param constraintDirection Direction of the same-station constraint (one of "above", "below", "left", "right",
+     *                            "above-left", "above-right", "below-left", "below-right", "equal").
+     */
+    private void writeSameStationConstraint(String station1Id, String station2Id, String constraintDirection) throws IOException {
+        // Only need 4 underlying constraint types, as we can reverse the order of station1 and station2
+        switch (constraintDirection) {
+            case "above" -> writeSameStationAboveConstraint(station1Id, station2Id);
+            case "below" -> writeSameStationAboveConstraint(station2Id, station1Id);
+
+            case "left" -> writeSameStationLeftConstraint(station1Id, station2Id);
+            case "right" -> writeSameStationLeftConstraint(station2Id, station1Id);
+
+            case "above-right" -> writeSameStationAboveRightConstraint(station1Id, station2Id);
+            case "below-left" -> writeSameStationAboveRightConstraint(station2Id, station1Id);
+
+            case "above-left" -> writeSameStationAboveLeftConstraint(station1Id, station2Id);
+            case "below-right" -> writeSameStationAboveLeftConstraint(station2Id, station1Id);
+
+            case "exact" -> writeSameStationEqualConstraint(station1Id, station2Id);
+
+            default -> throw new IllegalArgumentException(String.format("Unrecognized same-station constraint direction %s.", constraintDirection));
+        }
+    }
+
+    /**
+     * Processes a line of text which represents a 'same station' constraint. This is two stations that are directly
+     * adjacent on the map (usually representing multiple metro lines which stop at the same station).
+     * @param inputText The input text which declares the constraint, with "same-station" removed.
+     * @param metroLines Map from metro line name -> MetroLine object for the metro lines in the network.
+     * @param textLineNumber Line number of the input which declared this constraint.
+     */
+    private void processSameStationConstraint(String inputText, Map<String, MetroLine> metroLines, int textLineNumber) throws IOException {
+        if (inputText.charAt(0) == '"') {
+            // line is of the form "same-station \"line1Name: station1Name\" dir \"line2Name: station2Name\""
+            Pair<String, String> station1Result = Util.consumeDoubleQuoted(inputText, textLineNumber);
+            Pair<String, String> directionResult = Util.consumeToken(station1Result.getRight(), textLineNumber);
+            Pair<String, String> station2Result = Util.consumeDoubleQuoted(directionResult.getRight(), textLineNumber);
+
+            Pair<String, String> lineAndName1 = extractMetroLine(station1Result.getLeft(), textLineNumber);
+            String constraintDirection = directionResult.getLeft();
+            Pair<String, String> lineAndName2 = extractMetroLine(station2Result.getLeft(), textLineNumber);
+
+            String station1Id = metroLines.get(lineAndName1.getLeft())
+                    .getStation(lineAndName1.getRight(), textLineNumber).getAmplUniqueId();
+            String station2Id = metroLines.get(lineAndName2.getLeft())
+                    .getStation(lineAndName2.getRight(), textLineNumber).getAmplUniqueId();
+
+            writeSameStationConstraint(station1Id, station2Id, constraintDirection);
+        } else {
+            // line is of the form "same-station line1Name dir line2Name \"station1, station2, ...\""
+            Pair<String, String> token1Result = Util.consumeToken(inputText, textLineNumber);
+            Pair<String, String> token2Result = Util.consumeToken(token1Result.getRight(), textLineNumber);
+            Pair<String, String> token3Result = Util.consumeToken(token2Result.getRight(), textLineNumber);
+
+            String line1Name = token1Result.getLeft();
+            String constraintDirection = token2Result.getLeft();
+            String line2Name = token3Result.getLeft();
+
+            Pair<String, String> doubleQuotedResult = Util.consumeDoubleQuoted(token3Result.getRight(), textLineNumber);
+            String stationsString = doubleQuotedResult.getLeft();
+            stationsString = stationsString.strip();
+            String[] stations = stationsString.split(",");
+            stations = Arrays.stream(stations).map(String::strip).toArray(String[]::new); // some day, mapping a collection in Java won't be ugly
+
+            for (String stationName : stations) {
+                String station1Id = metroLines.get(line1Name).getStation(stationName, textLineNumber).getAmplUniqueId();
+                String station2Id = metroLines.get(line2Name).getStation(stationName, textLineNumber).getAmplUniqueId();
+                writeSameStationConstraint(station1Id, station2Id, constraintDirection);
+            }
         }
     }
 
@@ -202,7 +351,7 @@ public class AmplDriver {
         switch (constraintType) {
             case "vertical", "horizontal", "up-right", "down-right" ->
                     processCardinalDirectionConstraint(textLineRest, constraintType, metroLine, metroLines, textLineNumber);
-            case "same-station" -> { } // TODO
+            case "same-station" -> processSameStationConstraint(textLineRest, metroLines, textLineNumber);
             case "equal" -> { } // TODO
             default -> throw new IllegalArgumentException(String.format("Constraint (line %d) is invalid. Earlier code should have already validated this.", textLineNumber));
         }
--- scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/GenerateMap.java
+++ scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/GenerateMap.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 public class GenerateMap {
 
+    /** Line width of a metro line on the map, in pixels. */
+    private static final int metroLineWidth = 10;
+
     /**
      * args[0] = input file path
      * args[1] = naptan.json path
@@ -14,7 +17,7 @@ public class GenerateMap {
     public static void main(String[] args) throws IOException {
         Parser parser = new Parser(args[0], args[1]);
         var data = parser.parseData();
-        AmplDriver amplDriver = new AmplDriver(args[2]);
+        AmplDriver amplDriver = new AmplDriver(args[2], metroLineWidth);
         amplDriver.writeAmplFiles(args[3], args[4], data.getRight(), data.getLeft());
         System.out.println("Debugging.");
     }
