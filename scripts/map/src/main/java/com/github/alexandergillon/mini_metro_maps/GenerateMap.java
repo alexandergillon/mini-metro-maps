8f0137f6b7989cbf7737fb0baaf80f028a64b04a
Alex Gillon <alexandergillon@outlook.com>
2023-12-07 17:55:28 -0500

    Simplifies command-line arguments.

--- scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/GenerateMap.java
+++ scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/GenerateMap.java
@@ -1,6 +1,7 @@
 package com.github.alexandergillon.mini_metro_maps;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class GenerateMap {
 
@@ -10,15 +11,22 @@ public class GenerateMap {
     /**
      * args[0] = input file path
      * args[1] = naptan.json path
-     * args[2] = initial AMPL model path
-     * args[3] = output AMPL .mod path
-     * args[4] = output AMPL .dat path
+     * args[2] = AMPL directory path
+     * \initial_model.mod ampl\temp\model.mod ampl\temp\data.dat
      */
     public static void main(String[] args) throws IOException {
-        Parser parser = new Parser(args[0], args[1]);
+        String inputPath = args[0];
+        String naptanPath = args[1];
+        Path amplDir = Path.of(args[2]);
+
+        String amplInitialModelPath = amplDir.resolve("initial_model.mod").toString();
+        String amplModPath = amplDir.resolve("temp").resolve("model.mod").toString();
+        String amplDatPath = amplDir.resolve("temp").resolve("data.dat").toString();
+
+        Parser parser = new Parser(inputPath, naptanPath);
         var data = parser.parseData();
-        AmplDriver amplDriver = new AmplDriver(args[2], metroLineWidth);
-        amplDriver.writeAmplFiles(args[3], args[4], data.getRight(), data.getLeft());
+        AmplDriver amplDriver = new AmplDriver(amplInitialModelPath, metroLineWidth);
+        amplDriver.writeAmplFiles(amplModPath, amplDatPath, data.getRight(), data.getLeft());
         System.out.println("Debugging.");
     }
 }
