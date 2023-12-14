caf1c40651ac64cb0f2fea5990dc62e280f0b8e2
Alex Gillon <alexandergillon@outlook.com>
2023-12-14 15:01:20 -0500

    Transforms coordinates to be as far left/up as possible, with a border.

--- scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/models/core/AlignmentConstraint.java
+++ scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/models/core/AlignmentConstraint.java
@@ -1,4 +1,4 @@
 package com.github.alexandergillon.mini_metro_maps.models.core;
 
-/** POJO to hold information about a constraint. */
+/** POJO to hold information about an alignment constraint. */
 public record AlignmentConstraint(String constraintText, MetroLine metroLine, int textLineNumber) { }
