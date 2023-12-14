637e48628e8864a572f37e3f6c2173bfd38ead1a
Alex Gillon <alexandergillon@outlook.com>
2023-12-14 14:38:53 -0500

    Renames Constraint to AlignmentConstraint.

--- scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/models/core/AlignmentConstraint.java
+++ scripts/map/src/main/java/com/github/alexandergillon/mini_metro_maps/models/core/AlignmentConstraint.java
@@ -1,4 +1,4 @@
 package com.github.alexandergillon.mini_metro_maps.models.core;
 
 /** POJO to hold information about a constraint. */
-public record Constraint (String constraintText, MetroLine metroLine, int textLineNumber) { }
+public record AlignmentConstraint(String constraintText, MetroLine metroLine, int textLineNumber) { }
