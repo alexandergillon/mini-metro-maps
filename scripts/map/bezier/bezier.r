39298afacca5063ed037b5e6bb1bea69326bcd52
Alex Gillon <alexandergillon@outlook.com>
2023-12-16 15:08:09 -0500

    Cleans up R script.

--- scripts/map/bezier/bezier.r
+++ scripts/map/bezier/bezier.r
@@ -1,5 +1,15 @@
+# R script which takes in a cubic Bezier curve and a distance, and fits a parallel curve to the Bezier curve at that distance.
+# Fits curve on the outside of a the input curve. The fitted curve contains a number of cubic Bezier curves.
+#
+# Input is a 5x2 matrix in bezier_in.csv, where the first row is (distance, -1), and the next 4 rows are the Bezier control
+# points (x, y).
+#
+# Output is a (4n+1) x 2 matrix in bezier_out.csv containing n cubic Bezier curves. The first row is (n, -1), which allows you
+# to read what n is programatically. Each set of 4 rows following are control points (x, y) for a Bezier curve.
 library(bezier)
 
+# Returns the derivative of a cubic Bezier curve defined by (p0, p1, p2, p3) at input parameter t.
+# I.e. the tangent of the curve at t.
 cubic_bezier_derivative_at = function(p0, p1, p2, p3, t) {
     return(
         3 * ((1-t) ^ 2) * (p1 - p0)
@@ -8,11 +18,15 @@ cubic_bezier_derivative_at = function(p0, p1, p2, p3, t) {
     )
 }
 
+# Normalizes a vector.
 normalize = function(vector) {
     return(vector / sqrt(sum(vector^2)))
 }
 
-sample_points = function(control_points, n, width) {
+# Samples n points from a cubic Bezier curve defined by control_points at distance `distance` away.
+# Control points is a 4x2 matrix where each row is a control point (x, y).
+# No guarantee on distribution of sample points (todo: improve?).
+sample_points = function(control_points, n, distance) {
     p0 = control_points[1, 1:2]
     p1 = control_points[2, 1:2]
     p2 = control_points[3, 1:2]
@@ -26,7 +40,7 @@ sample_points = function(control_points, n, width) {
 
         tangent_vector = matrix(cubic_bezier_derivative_at(p0, p1, p2, p3, t), nrow=1, ncol=2)
         normal_vector_norm = normalize(matrix(c(tangent_vector[2], -tangent_vector[1]), nrow=1, ncol=2))
-        normal_vector = normal_vector_norm * width
+        normal_vector = normal_vector_norm * distance
 
         normal_point_1 = bezier(t=t, p=control_points) - normal_vector
         normal_point_2 = normal_point_1 + 2 * normal_vector
@@ -38,6 +52,7 @@ sample_points = function(control_points, n, width) {
     return(points)
 }
 
+# Converts the output of bezierCurveFit to a 4x2 matrix of control points, where each row is a control point (x, y).
 bezier_output_to_matrix = function(bezier_output) {
     p = bezier_output$p
     return(matrix(
@@ -45,6 +60,7 @@ bezier_output_to_matrix = function(bezier_output) {
     ))
 }
 
+# Reads the input from csv file.
 read_input = function() {
     csv = read.csv("bezier_in.csv", header = FALSE)
     input = as.matrix(csv)
@@ -54,34 +70,24 @@ main = function() {
     num_sample_points = 100
     input = read_input()
 
+    # Input has a header row, which is (distance, -1) 
     line_width = input[1][1]
+    # The next 4 rows are control points of a cubic Bezier curve.
     control_points = input[2:5, 1:2]
     
-    t = seq(0, 1, length=num_sample_points)
-    plot(bezier(t=t, p=control_points), asp=1)
-
     sampled_points = sample_points(control_points, num_sample_points, line_width)
 
-    for (i in 1:num_sample_points) {
-        points(sampled_points[i, 1], sampled_points[i, 2], col=2)
-    }
-
-    print(nrow(sampled_points))
-    first_half = sampled_points[1:num_sample_points %/% 2, 1:2]
-    second_half = sampled_points[51 : 100, 1:2]
+    # For now, we are hardcoding fitting the curve with 2 cubic Bezier curves.
+    first_half =  sampled_points[1                       : num_sample_points %/% 2 , 1 : 2]
+    second_half = sampled_points[num_sample_points %/% 2 : num_sample_points       , 1 : 2]
 
     fitted_curve_1 = bezierCurveFit(first_half, min.control.points=4, max.control.points=4, fix.start.end=TRUE)
     fitted_curve_2 = bezierCurveFit(second_half, min.control.points=4, max.control.points=4, fix.start.end=TRUE)
-    points(bezier(t=t, p=fitted_curve_1$p), asp=1, col=3)
-    points(bezier(t=t, p=fitted_curve_2$p), asp=1, col=3)
 
-    print(fitted_curve_1)
-    print(bezier_output_to_matrix(fitted_curve_1))
-
-    header = matrix(c(55, 66), nrow=1, ncol=2)
-    print(rbind(header, bezier_output_to_matrix(fitted_curve_1), bezier_output_to_matrix(fitted_curve_2)))
+    header = matrix(c(2, -1), nrow=1, ncol=2)
+    output = rbind(header, bezier_output_to_matrix(fitted_curve_1), bezier_output_to_matrix(fitted_curve_2))
+    print(output)
+    write.table(output, file="bezier_out.csv", sep = ",", row.names = FALSE, col.names = FALSE)
 }
 
 main()
-
-#print(fitted_curve_2)
