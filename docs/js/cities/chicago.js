de55a911e18975f8f4b1dfe2329e0f852bae0d19
Alex Gillon <alexandergillon@outlook.com>
2024-11-26 20:10:00 -0600

    Adds Chicago frontend skeleton.

---
+++ docs/js/cities/chicago.js
@@ -0,0 +1,18 @@
+/**
+ * Sets the metro lines which arrivals should be fetched for.
+ * @param metroLines Names of the metro lines, as an array of strings.
+ */
+function setLines(metroLines) {
+    // TODO
+}
+/**
+ * Gets the next arrival for all trains on lines previously specified by setLines().
+ * Each train has only 1 NextArrivalInfo in the return value.
+ * @returns The next arrival of each train on the previously configured lines.
+ */
+async function getData() {
+    // todo: error handling
+    console.log("getData"); // todo: remove
+    throw new Error("unimplemented");
+}
+export { setLines, getData };
